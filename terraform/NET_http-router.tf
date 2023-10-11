cddbfeb53989829fc9dd0b26540745506cc52680
yuliya8 <131459320+yuliya8@users.noreply.github.com>
2023-10-12 02:55:14 +0300

    Add files via upload

---
+++ terraform/NET_http-router.tf
@@ -0,0 +1,26 @@
+# Terraform configuration for HTTP router
+##### Создайте HTTP router. Путь укажите — /, backend group — созданную ранее.
+###### http_match - (Optional) Checks "/" prefix by default.
+
+resource "yandex_alb_http_router" "diploma-http-router" {
+  name = "diploma-http-router"
+}
+
+resource "yandex_alb_virtual_host" "http-router-virtual-host" {
+  name = "http-router-virtual-host"
+  http_router_id = yandex_alb_http_router.diploma-http-router.id
+  route {
+    name = "diploma-route"
+    http_route {
+#optional      http_match {
+#optional        path {
+#optional          prefix = "/"
+#optional        }  
+#optional      }
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.diploma-backend-group.id
+#optional        timeout = "60s"
+      }
+    }
+  }
+}
---
+++ terraform/NET_security-groups.tf
@@ -0,0 +1,137 @@
+#Terraform configuration for Security Groups
+
+## -----Security Group Bastion host-----
+
+resource "yandex_vpc_security_group" "SG-bastion-host" {
+  name        = "SG-bastion-host"
+  description = "This rule allows SSH connection and any egress traffic for bastion host"
+  network_id  = yandex_vpc_network.diploma-network.id
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## -----Security Group SSH and ICMP traffic-----
+
+resource "yandex_vpc_security_group" "SG-ssh-icmp-traffic" {
+  name        = "SG-ssh-icmp-traffic"
+  description = "This rule allows SSH access from the bastion host's internal interface IPs to the internal hosts"
+  network_id  = yandex_vpc_network.diploma-network.id
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = ["10.153.0.0/24"]
+  }
+  ingress {
+    protocol       = "ICMP"
+    v4_cidr_blocks = ["10.153.0.0/24"]
+  }
+}
+
+## -----Security Group Web-servers-----
+
+resource "yandex_vpc_security_group" "SG-web-servers" {
+  name        = "SG-web-servers"
+  network_id  = yandex_vpc_network.diploma-network.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 80
+    v4_cidr_blocks = ["10.153.0.0/24"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    port           = 10050
+    v4_cidr_blocks = ["10.153.0.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## -----Security Group Zabbix-----
+
+resource "yandex_vpc_security_group" "SG-zabbix" {
+  name        = "SG-zabbix"
+  network_id  = yandex_vpc_network.diploma-network.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 80
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    port           = 10051
+    v4_cidr_blocks = ["10.150.0.0/24", "10.151.0.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+## -----Security Group ElasticSearch-----
+
+resource "yandex_vpc_security_group" "SG-elasticsearch" {
+  name        = "SG-elasticsearch"
+  network_id  = yandex_vpc_network.diploma-network.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 9200
+    v4_cidr_blocks = ["10.150.0.0/24", "10.151.0.0/24", "10.153.0.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## -----Security Group Kibana-----
+
+resource "yandex_vpc_security_group" "SG-kibana" {
+  name        = "SG-kibana"
+  network_id  = yandex_vpc_network.diploma-network.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 5601
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## -----Security Group Application load balancer-----
+
+resource "yandex_vpc_security_group" "SG-load-balanser" {
+  name        = "SG-load-balanser"
+  network_id  = yandex_vpc_network.diploma-network.id
+
+  ingress {
+    protocol       = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
