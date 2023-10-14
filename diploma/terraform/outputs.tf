0d4c1ff8aac69604abfbee281b75a2e2bc40b48b
yuliya8 <131459320+yuliya8@users.noreply.github.com>
2023-10-15 00:42:59 +0300

    diploma 14-15/10/2023
    открытые вопросы:
    - роль заббикс
    - роль эластик
    - роль кибана
    - проверка роли нджинкс
    - плэйбук ансибла
    - проверка конфигурации ансибла
    - м.б. какая-либо проверка результатов
    - хэлс чек вебсерверов лоадбалансером
    - прочие украшательства скрипта
    - обновление скрипта с планом б относительно основного скрипта

---
+++ diploma/terraform/outputs.tf
@@ -0,0 +1,32 @@
+output "_ssh-config" {
+  description = "Is used to create SSH configuration file using template"
+  value = templatefile("my_ssh_config.tpl", {
+    bastion-public-ip = yandex_compute_instance.bastion-host.network_interface.0.nat_ip_address,
+    elasticsearch-private-ip = yandex_compute_instance.elasticsearch-server.network_interface.0.ip_address,
+    kibana-private-ip = yandex_compute_instance.kibana-server.network_interface.0.ip_address,
+    zabbix-private-ip = yandex_compute_instance.zabbix-server.network_interface.0.ip_address,
+    web-server-1-private-ip = yandex_compute_instance.web-server-1.network_interface.0.ip_address,
+    web-server-2-private-ip = yandex_compute_instance.web-server-2.network_interface.0.ip_address
+  })
+}
+
+output "ansible-inventory" {
+  description = "Is used to create ansible inventory file using template"
+  value = templatefile("my_ansible_inventory.tpl", {
+    bastion-public-ip = yandex_compute_instance.bastion-host.network_interface.0.nat_ip_address,
+    elasticsearch-private-ip = yandex_compute_instance.elasticsearch-server.network_interface.0.ip_address,
+    kibana-private-ip = yandex_compute_instance.kibana-server.network_interface.0.ip_address,
+    zabbix-private-ip = yandex_compute_instance.zabbix-server.network_interface.0.ip_address,
+    web-server-1-private-ip = yandex_compute_instance.web-server-1.network_interface.0.ip_address,
+    web-server-2-private-ip = yandex_compute_instance.web-server-2.network_interface.0.ip_address
+  })
+}
+
+output "public-ip-addresses" {
+  value = <<OUTPUT
+Application load balancer = ${yandex_alb_load_balancer.diploma-load-balancer.listener.0.endpoint.0.address.0.external_ipv4_address.0.address}
+Zabbix server = ${yandex_compute_instance.zabbix-server.network_interface.0.nat_ip_address}
+Kibana server = ${yandex_compute_instance.kibana-server.network_interface.0.nat_ip_address}
+Bastion host = ${yandex_compute_instance.bastion-host.network_interface.0.nat_ip_address}
+OUTPUT
+}
