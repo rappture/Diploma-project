7d982051822cece3af57eaba91d8588e1b1561d8
yuliya8 <131459320+yuliya8@users.noreply.github.com>
2023-10-20 04:41:15 +0300

    Add files via upload

---
+++ diploma/terraform/NET_backend_group.tf
@@ -0,0 +1,30 @@
+# Terraform configuration for web-servers Backend Group
+###### Создайте Backend Group, настройте backends на target group, ранее созданную. Настройте healthcheck на корень (/) и порт 80, протокол HTTP.
+
+resource "yandex_alb_backend_group" "diploma-backend-group" {
+  name = "diploma-backend-group"
+#optional  session_affinity {
+#optional    connection {
+#optional      source_ip = <true_OR_false>
+#optional    }
+#optional  }
+
+  http_backend {
+    name = "diploma-backend"
+#optional    weight = 1
+    port = 80
+    target_group_ids = ["${yandex_alb_target_group.diploma-target-group.id}"]
+#optional    load_balancing_config {
+#optional      panic_threshold = 90
+#optional    } 
+    healthcheck {
+      timeout = "10s"
+      interval = "2s"
+#optional      healthy_threshold = 10
+#optional      unhealthy_threshold = 15 
+      http_healthcheck {
+        path = "/"
+      }
+    }
+  }
+}
